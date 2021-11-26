d254284dcb5480603ffbbab4ed350f731671a97e
Claire Froelich <claire.froelich@gmail.com>
2021-11-26 12:13:00 -0800

    Correct the docker setup for testing

--- api/src/directives/requireAuth/requireAuth.test.ts
+++ api/src/directives/requireAuth/requireAuth.test.ts
@@ -2,10 +2,9 @@ import { mockRedwoodDirective, getDirectiveName } from '@redwoodjs/testing/api'
 
 import requireAuth from './requireAuth'
 
-describe.only('requireAuth directive', () => {
+describe('requireAuth directive', () => {
   it('declares the directive sdl as schema, with the correct name', () => {
     expect(requireAuth.schema).toBeTruthy()
-    console.log({schema: requireAuth.schema.definitions})
     expect(getDirectiveName(requireAuth.schema)).toBe('requireAuth')
   })
 
@@ -13,7 +12,6 @@ describe.only('requireAuth directive', () => {
     // If you want to set values in context, pass it through e.g.
     // mockRedwoodDirective(requireAuth, { context: { currentUser: { id: 1, name: 'Lebron McGretzky' } }})
     const mockExecution = mockRedwoodDirective(requireAuth, { context: {} })
-    console.log({mockExecution: mockExecution()})
 
     expect(mockExecution).not.toThrowError()
   })
